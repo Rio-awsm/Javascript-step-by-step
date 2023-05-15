14f84550773b0cac61e912a26dc1d59f7414a955
Supriyo Saha <mm.saha1982@gmail.com>
2023-05-15 12:56:49 +0530

    chapter update

---
+++ 03_operators-expressions.js
@@ -0,0 +1,29 @@
+console.log("Arithmatic operators")
+let a = 5;
+let b = 3;
+console.log("a+b = ", a+b)
+console.log("a-b = ", a-b)
+console.log("a*b = ", a*b)
+console.log("a/b = ", a/b)
+console.log("a^b = ", a**b)
+console.log("a%b = ", a%b)
+console.log(a++)
+console.log(++a)
+console.log(a--)
+console.log(--a)
+
+console.log("Assingment operators in js")
+b=8
+console.log(b)
+a+=5
+console.log(b)
+
+console.log("Comparison operators")
+console.log(a==b)
+console.log(a!=b)
+console.log(a===b)
+console.log(a!==b)
+
+console.log("Logocal operators")
+console.log(a>b && a==5)
+console.log(a>b || a==5)
