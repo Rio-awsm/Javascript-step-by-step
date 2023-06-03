79d5b6a8b19102b4551ad7d4fc09961b9ad6c60d
Supriyo Saha <mm.saha1982@gmail.com>
2023-06-03 17:58:29 +0530

    functions added

---
+++ 07_functions.js
@@ -0,0 +1,20 @@
+function onePlusAvg(x, y){
+    return Math.round(1 + (x + y)/2)
+}
+
+const sum = (p,q) =>{
+    return p + q
+}
+
+const hello = () =>{
+    console.log("Hello !");
+}
+
+let a= 2;
+let b = 3;
+let c = 13;
+
+hello()
+
+console.log(onePlusAvg(a,c));
+console.log(sum(b,a));
