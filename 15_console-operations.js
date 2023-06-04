87adbb09caa7682ff9e8ffc6fcf6882f4f1ac8b8
Supriyo Saha <mm.saha1982@gmail.com>
2023-06-04 22:09:45 +0530

    added chapter

---
+++ 15_console-operations.js
@@ -0,0 +1,13 @@
+console.assert(5 > 6);
+// console.clear()
+obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+console.table(obj)
+console.warn("Stop!!")
+console.info("This is info")
+console.error("err")
+console.log(console);
+
---
+++ practice3.js
@@ -0,0 +1,2 @@
+//generate a random number and store it in a variable. The program then takes a input from user and check if the guess is equal to the random number or not. Provide score to the player and it terminates once the guess is right. Limit of max number is 100.
+
